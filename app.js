e9271fa87cac5317402e37da30b0a4b8fe765b2b
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-22 17:45:01 +0200

    Express | Layouts & Partials

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let counter = 0;
 
@@ -8,6 +9,10 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
+// Register the partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 app.use(express.static('public'));
 
 
@@ -15,8 +20,8 @@ app.get('/', (req, res, next) => {
   counter++;
   let homes = [
     { city: "Berlin", address: "Markgrafendam 28", isWeWork: false },
-    { city: "Paris", address: "33 rue La Fayette", isWeWork: true },
-    { city: "Berlin", address: "Atrium Tower", isWeWork: true }
+    { city: "Paris", address: "33 rue La Fayette", isWeWork: true, picture: "https://cdn.wework.com/locations/image/cb34f312-faf2-11e7-989d-1202be33576a/20170405_WeWork_La_Fayette-Paris_Benoit_Florencon-high_08.jpg?auto=compress%2Cformat&w=1200&h=600&fit=crop" },
+    { city: "Berlin", address: "Atrium Tower", isWeWork: true, picture: "/images/wework-berlin.jpg" }
   ]
   res.render('index', {
     counter: counter,
