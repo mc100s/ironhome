78c37b31881248463784f5be6016eb26a0db21b6
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-22 17:00:38 +0200

    Express | Layouts & Partials

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
 
+let counter = 0;
+
 const app = express();
+
+// Set HBS as our view engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 
-app.get('/', (request, response, next) => {
-  response.sendFile(__dirname + '/views/home-page.html');
+
+app.get('/', (req, res, next) => {
+  counter++;
+  let homes = [
+    { city: "Berlin", address: "Markgrafendam 28", isWeWork: false },
+    { city: "Paris", address: "33 rue La Fayette", isWeWork: true },
+    { city: "Berlin", address: "Atrium Tower", isWeWork: true }
+  ]
+  res.render('index', {
+    counter: counter,
+    homes: homes
+  });
 });
 
-app.get('/best-homes', (request, response, next) => {
-  response.sendFile(__dirname + '/views/houses-page.html');
+app.get('/best-homes', (req, res, next) => {
+  res.render('homes');
 });
+
+app.get('/login', (req,res,next) => {
+  res.render('login');
+})
 
 
 app.listen(3000, () => {
