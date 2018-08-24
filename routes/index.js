eb7178c44aa39a5b6f93cb7bb8d1b910e0ac886f
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-24 18:09:33 +0200

    Add of add page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const Home = require("../models/Home")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -9,5 +10,39 @@ router.get('/', (req, res, next) => {
 router.get('/test', (req, res, next) => {
   res.render('test');
 });
+
+router.get('/homes', (req, res, next) => {
+  Home.find()
+  .populate('_owner')
+  .then(homes => {
+    res.render('homes', {
+      homes
+    });
+  })
+});
+
+router.get('/homes/add', (req, res, next) => {
+  res.render('add-home');
+})
+
+router.post('/homes/add', (req, res, next) => {
+  console.log("req.body", req.body);
+  let newHome = {
+    picture: req.body.picture,
+    address: {
+      street: req.body.street,
+      city: req.body.city,
+    }
+  }
+  Home.create(newHome)
+  .then(home => {
+    console.log("The home was saved!!!");
+    res.redirect('/homes/' + home._id);
+  })
+})
+
+router.get('/homes/:id', (req, res, next) => {
+  res.send('In construction');
+})
 
 module.exports = router;
